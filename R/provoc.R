6a8e20a2da9016817e9af04b0f226c785ffd359a
DBecker7 <d7becker@gmail.com>
2022-04-07 17:52:38 -0400

    New function: Begin grand design

---
+++ R/provoc.R
@@ -0,0 +1,40 @@
+#' Proportions of Variants of Concern
+#' 
+#' Un-fuses coco and varmat and applies the appropriate estimation technique. Applies to each unique value of 
+#' 
+#' @param fused The fused data frame of coco and varmat
+#' @param method Optimization or Bayesian?
+#' 
+#' @return Results of the estimation. Currently has different output depending on method, but will hopefully be unified in a later version.
+#' @export
+provoc <- function (fused, method = c("optim", "runjags")) {
+    # TODO: Respect the sample column (output a list.)
+    # TODO: Allow parameters to be passed to the respective functions.
+    variants <- startsWith(names(fused), "var_")
+    coco <- fused[, !variants]
+
+    vardf <- fused[, variants]
+    varmat <- t(as.matrix(vardf))
+    varmat <- matrix(as.numeric(varmat), ncol = ncol(varmat))
+    rownames(varmat) <- gsub("var_", "", names(coco)[variants])
+    colnames(varmat) <- coco$mutation
+
+    if(method[1] == "optim") {
+        res <- copt_binom(coco, varmat)
+        # TODO: Take best version of nuclear option
+        # TODO: Error checking in *_binom are unnecessary
+        # TODO: Process the output
+        # TODO: Better names for everything (these functions will still be exported if users want to perform their own error checks, make them easy to use; maybe provoc_optimize?).
+    } else {
+        res <- coda_binom(coco, varmat)
+        # TODO: rename p[x] with the voc name
+        # TODO: Inits and seeds
+        # TODO: Process the output
+    }
+    # TODO: Unify output formats, including convergence yes/no, convergence note and point estimates.
+    # TODO: (Long Term) make this it's own class with nice printing defaults
+    res
+}
+
+
+
