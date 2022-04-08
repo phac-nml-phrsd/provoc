9f13aad2360a1d1f9fbc279c31be060c221dbfe7
DBecker7 <d7becker@gmail.com>
2022-04-08 11:07:19 -0400

    Better output and argument handling

--- R/provoc.R
+++ R/provoc.R
@@ -4,31 +4,106 @@
 #' 
 #' @param fused The fused data frame of coco and varmat
 #' @param method Optimization or Bayesian?
+#' @param ... Arguments to be passed to provoc_jags (ignored for method = "optim"). See \code{?provoc_binom}.
 #' 
 #' @return Results of the estimation. Currently has different output depending on method, but will hopefully be unified in a later version.
 #' @export
-provoc <- function (fused, method = c("optim", "runjags")) {
-    # TODO: Respect the sample column (output a list.)
-    # TODO: Allow parameters to be passed to the respective functions.
-    variants <- startsWith(names(fused), "var_")
-    coco <- fused[, !variants]
-
-    vardf <- fused[, variants]
-    varmat <- t(as.matrix(vardf))
-    varmat <- matrix(as.numeric(varmat), ncol = ncol(varmat))
-    rownames(varmat) <- gsub("var_", "", names(coco)[variants])
-    colnames(varmat) <- coco$mutation
-
-    if(method[1] == "optim") {
-        res <- provoc_optim(coco, varmat)
-        # TODO: Process the output
+provoc <- function (fused, method = c("optim", "runjags"), ...) {
+    if("sample" %in% colnames(fused)) {
+        samples <- unique(fused$sample)
+        sample_table <- table(fused$sample)
+        if(any(sample_table < 5)) {
+            if(mean(sample_table < 5) < 0.5) {
+                print(sample_table)
+                stop("Too few samples")
+            } else {
+                samples <- names(sample_table[sample_table > 5])
+                warning("Some samples have fewer than 5 observations and have been removed from analysis.")
+                print(table(samples))
+            }
+        }
+        if(any(sample_table < 10)) {
+            warning("At least one of the samples has fewer than 10 observations.")
+            print(sample_table)
+        }
     } else {
-        res <- provoc_jags(coco, varmat)
-        # TODO: Process the output
+        fused$sample <- 1
+        samples <- 1
     }
-    # TODO: Unify output formats, including convergence yes/no, convergence note and point estimates.
-    # TODO: (Long Term) make this it's own class with nice printing defaults
-    res
+
+    res_list <- vector(mode = "list", length = length(samples))
+    names(res_list) <- samples
+    for(i in seq_along(res_list)) {
+        # TODO: Allow parameters to be passed to the respective functions.
+        cat("\n")
+        message(paste0("Fitting sample ", samples[i], ", ", which(samples == samples[i]), "of ", length(samples)))
+        fusi <- fused[fused$sample == samples[i], ]
+        variants <- startsWith(names(fusi), "var_")
+        varnames <- names(fused)[variants]
+        coco <- fusi[, !variants]
+
+        vardf <- fusi[, variants]
+        varmat <- t(as.matrix(vardf))
+        varmat <- matrix(as.numeric(varmat), ncol = ncol(varmat))
+        rownames(varmat) <- gsub("var_", "", varnames)
+        colnames(varmat) <- coco$mutation
+
+        if(method[1] == "optim") {
+            res_temp <- provoc_optim(coco, varmat)
+            res <- list(
+                point_est = data.frame(rho = res_temp$par, 
+                    variant = varnames,
+                    ci_low = NA,
+                    ci_high = NA),
+                convergence = res_temp$convergence,
+                convergence_note = res_temp$init_method,
+                note = "CI is NA; bootstrapping not yet implemented.",
+                logLik = -res_temp$value)
+        } else {
+            if(requireNamespace("runjags", quietly = TRUE)) {
+                res_temp <- provoc_jags(coco, varmat, ...)
+
+                if(requireNamespace("coda", quietly = TRUE)) {
+                    gr <- coda::gelman.diag(res_temp)$psrf
+                    convergence_gr <- coda::gelman.diag(res_temp)$mpsrf > 1.15
+                } else {
+                    gr <- "coda not install, cannot calculate GR statistics"
+                    convergence_gr <- NA
+                }
+
+                point_est <- melt_mcmc(res_temp, varmat, pivot = FALSE)
+                point_est <- point_est[, -which(names(point_est) %in% c("chain", "iter"))]
+                point_est <- t(apply(point_est, 2, quantile, probs = c(0.025, 0.5, 0.975)))
+                point_est <- as.data.frame(point_est)
+                names(point_est) <- c("ci_low", "rho", "ci_high")
+                point_est$variant <- rownames(varmat)
+                point_est <- point_est[, c("rho", "ci_low", "ci_high", "variant")]
+                rownames(point_est) <- NULL
+
+                logLik <- sum(stats::dbinom(
+                    x = fused$count, 
+                    size = fused$coverage, 
+                    prob = as.numeric(point_est$rho %*% varmat), 
+                    log = TRUE))
+
+                res <- list(
+                    point_est = point_est,
+                    convergence = convergence_gr,
+                    convergence_note = gr,
+                    note = "To return entire posteriors, call provoc_binom() directly. Loglik is calculated for medians only. Convergence is based on multivariate psrf.",
+                    logLik = logLik)
+                    
+            } else {
+                res <- "JAGS and runjags are required to use this method."
+            }
+        }
+        # TODO: (Long Term) make this it's own class with nice printing defaults
+        res_list[[i]] <- res
+
+        message("Done.")
+    }
+
+    res_list
 }
 
 
--- R/provoc_jags.R
+++ R/provoc_jags.R
@@ -29,6 +29,7 @@ melt_mcmc <- function(mcmc.list, var_names = NULL, pivot = FALSE) {
 #' @param varmat The variant matrix to be used in the study. The rownames must be the VoCs and the colnames must be the mutation names (in the same format as the mutation names in `coco`)
 #' @param adapt,burnin,sample,thin Parameters passed to runjags. Note that \code{sample} is the final number of samples that you will receive from each chain (i.e. if \code{thin = 5} and \code{sample = 1000}, you will end up drawing 5000 samples so that 1000 are returned after thinning).
 #' @param quiet If TRUE, much (but not all) of the runjags output will be suppressed.
+#' @param summarise Include runjags' summaries of the MCMC output.
 #' 
 #' @return an mcmc.list object with each column representing the proportion of the variant of concern, in the order of the rownames of variantmat. It is suggested to use melt_mcmc to get output that plays better with ggplot2 and dplyr.
 #' @export
@@ -44,65 +45,62 @@ melt_mcmc <- function(mcmc.list, var_names = NULL, pivot = FALSE) {
 #' 
 provoc_jags <- function(
         coco, varmat, 
-        adapt = 500, burnin = 1000, sample = 1000, thin = 4,
-        quiet = TRUE){
-    if(requireNamespace("runjags", quietly = TRUE)) {
-        muts <- coco$mutation
-        cou2 <- coco$count
-        cov2 <- coco$coverage
-        vari2 <- varmat
+        adapt = 500, burnin = 1000, thin = 4, 
+        sample = 1000, n.chains = 3,
+        quiet = TRUE, summarise = FALSE){
+    muts <- coco$mutation
+    cou2 <- coco$count
+    cov2 <- coco$coverage
+    vari2 <- varmat
 
-        jags_rho_inits <- function(varmat) {
-            method = round(runif(1))
-            if(method == 1) {
-                init1 <- rho_initializer(varmat)
-                init1 <- init1 + rnorm(length(init1), 0, 0.05)
-                return(to_feasible(init1))
-            } else {
-                init2 <- runif(nrow(varmat), 0.2, 0.8)
-                return(to_feasible(init2))
-            }
+    jags_rho_inits <- function(varmat) {
+        method = round(runif(1))
+        if(method == 1) {
+            init1 <- rho_initializer(varmat)
+            init1 <- init1 + rnorm(length(init1), 0, 0.05)
+            return(to_feasible(init1))
+        } else {
+            init2 <- runif(nrow(varmat), 0.2, 0.8)
+            return(to_feasible(init2))
         }
-
-        res <- tryCatch(
-                runjags::run.jags(
-                    model = system.file("extdata/provoc.JAGS",
-                     package = "provoc"),
-                    data = list(
-                        count = cou2,
-                        N = length(cou2),
-                        coverage = cov2,
-                        P = nrow(vari2),
-                        variantmat = vari2,
-                        alpha = 2,
-                        beta = 8),
-                    inits = list(
-                        list(p1 = jags_rho_inits(varmat), 
-                            .RNG.name="base::Super-Duper", 
-                            .RNG.seed = 1), 
-                        list(p1 = jags_rho_inits(varmat), 
-                            .RNG.name="base::Wichmann-Hill", 
-                            .RNG.seed = 2), 
-                        list(p1 = jags_rho_inits(varmat), 
-                            .RNG.name="base::Mersenne-Twister", 
-                            .RNG.seed = 3)
-                    ),
-                    adapt = adapt,
-                    burnin = burnin,
-                    n.chains = 3,
-                    sample = sample,
-                    thin = thin,
-                    monitor = c("p"),
-                    summarise = FALSE,
-                    silent = quiet,
-                    #silent.runjags = TRUE,
-                    method = "parallel"),
-                error = function(e) e)
-        for(i in 1:length(res$mcmc)) {
-            colnames(res$mcmc[[i]]) <- rownames(vari2)
-        }
-        return(res)
-    } else {
-        "JAGS and runjags are required to use this function."
     }
+
+    res <- tryCatch(
+            runjags::run.jags(
+                model = system.file("extdata/provoc.JAGS",
+                 package = "provoc"),
+                data = list(
+                    count = cou2,
+                    N = length(cou2),
+                    coverage = cov2,
+                    P = nrow(vari2),
+                    variantmat = vari2,
+                    alpha = 2,
+                    beta = 8),
+                inits = list(
+                    list(p1 = jags_rho_inits(varmat), 
+                        .RNG.name="base::Super-Duper", 
+                        .RNG.seed = 1), 
+                    list(p1 = jags_rho_inits(varmat), 
+                        .RNG.name="base::Wichmann-Hill", 
+                        .RNG.seed = 2), 
+                    list(p1 = jags_rho_inits(varmat), 
+                        .RNG.name="base::Mersenne-Twister", 
+                        .RNG.seed = 3)
+                ),
+                adapt = adapt,
+                burnin = burnin,
+                n.chains = n.chains,
+                sample = sample,
+                thin = thin,
+                monitor = c("p"),
+                summarise = summarise,
+                silent = quiet,
+                #silent.runjags = TRUE,
+                method = "parallel"),
+            error = function(e) e)
+    for(i in 1:length(res$mcmc)) {
+        colnames(res$mcmc[[i]]) <- rownames(vari2)
+    }
+    return(res)
 }
