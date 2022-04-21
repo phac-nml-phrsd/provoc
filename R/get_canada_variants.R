3f589cb107b622d5572e7a04e9ba74f915d8c76f
DBecker7 <d7becker@gmail.com>
2022-04-21 10:30:12 -0400

    New function: get lineage info from PANGO

--- R/get_canada_variants.R
+++ R/get_canada_variants.R
@@ -1,16 +1,17 @@
 #' Download weekly breakdown of SARS-CoV-2 variants in Canada
 #' 
 #' @param path Where to read the file (or store it if not already downloaded). Relative paths are fine. I suggest using the \code{here} package to get full path names so this function can do better error checking.
+#' @param force Download the data whether it exists or not (please be kind to the serves).
 #' 
 #' @return A data frame with weekly variant numbers
 #' @export
-get_canada_variants <- function(path = "data/canada_variants.csv"){
+get_canada_variants <- function(path = "data/canada_variants.csv", force = FALSE){
     directories <- strsplit(path, "/")[[1]]
     file_dir <- paste(directories[1:(length(directories) - 1)])
     if(!dir.exists(file_dir)) {
         stop(paste0("I cannot find the directory ''", file_dir, "''"))
     }
-    if(file.exists(path)) {
+    if(file.exists(path) & !force) {
         canada_variants <- read.csv(path)
     } else {
         canada_variants <- read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-variants.csv")
