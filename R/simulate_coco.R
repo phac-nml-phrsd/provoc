e47a039e36abef011145977d44474b8cbf16a15f
DBecker7 <d7becker@gmail.com>
2022-05-13 17:10:46 -0400

    Less talk more rock

--- R/simulate_coco.R
+++ R/simulate_coco.R
@@ -3,13 +3,14 @@
 #' @param varmat The variant matrix, with properly named rows (VOCs) and columns (mutations)
 #' @param rel_counts (Optional) The relative counts of each VOC (will be censored by coverage). Must be same length as nrow(varmat).
 #' @param censoring (Optional) The proportion of observations for each mutation. Must be same length as ncol(varmat).
+#' @param verbose Print information about the simulation
 #' @param absurd If true, the counts are completely unrelated to the variant proportions. Useful for Monte Carlo estimation.
 #' 
 #' @return A data frame with counts and coverage of the sampled mutations, ready to be used in \code{copt_binom()}.
 #' @export
 #' 
 #' @details By default, uses a negative binomial distribution for the counts and uniform censoring for the coverage.
-simulate_coco <- function(varmat, rel_counts = NULL, censoring = NULL, absurd = FALSE) {
+simulate_coco <- function(varmat, rel_counts = NULL, censoring = NULL, absurd = FALSE, verbose = TRUE) {
     if(is.null(rel_counts)) {
         rel_counts <- stats::rnbinom(nrow(varmat), mu = 25, size = 100)
         max_count <- max(rel_counts)
@@ -19,9 +20,11 @@ simulate_coco <- function(varmat, rel_counts = NULL, censoring = NULL, absurd = 
             stop("Counts must correspond to rows in varmat.")
         }
     }
-    print("Expected results:")
-    print(rbind(variant = rownames(varmat), proportion = round(rel_counts/sum(rel_counts), 3)))
-
+    if(verbose) {
+        print("Expected results:")
+        print(rbind(variant = rownames(varmat), 
+            proportion = round(rel_counts/sum(rel_counts), 3)))
+    }
     if(is.null(censoring)) {
         censoring <- stats::runif(ncol(varmat))
     } else {
