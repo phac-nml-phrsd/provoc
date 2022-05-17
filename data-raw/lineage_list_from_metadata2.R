ba43c746522350ca40c772e6196748a9d42ac6dc
DBecker7 <d7becker@gmail.com>
2022-05-17 12:14:54 -0400

    Keep zip, read in larger chunks

--- data-raw/lineage_list_from_metadata2.R
+++ data-raw/lineage_list_from_metadata2.R
@@ -2,62 +2,60 @@ library(lubridate)
 library(dplyr)
 
 # wget https://data.nextstrain.org/files/ncov/open/metadata.tsv.gz
-# gunzip -k metadata.tsv.gz
-handle <- file("metadata.tsv", "r")
+handle <- gzfile("metadata.tsv.gz", "r")
 header <- readLines(handle, n = 1)
 
 # For loops in R are not slow!
 # (Unless, of course, you're growing a list, which is exactly what I'm doing.)
 ll <- list()
 counter <- 0
+t0 <- Sys.time()
 while(TRUE){
-    counter <- counter + 1
-    if (!counter %% 5000) print(counter)
-    mtpi <- readLines(handle, n = 1)
-    if(length(mtpi) == 0) {
-        break
-    }
-    mtpi <- strsplit(mtpi, "\t")[[1]]
-
-    lineage <- mtpi[20]
-    mutations <- unlist(strsplit(mtpi[46:48], ","))
-    date <- as.character(mtpi[28])
-    country <- mtpi[8]
-
-
-    if(!lineage %in% names(ll)) {
-        ll[[lineage]] <- vector(mode = "list", length = length(mutations))
-        names(ll[[lineage]]) <- mutations
-        for(mut in mutations) {
-            ll[[lineage]][[mut]] <- list(
-                count = 1, 
-                date1 = date, date2 = date, daten2 = date, daten1 = date)
+    mtp <- readLines(handle, n = 1000)
+    for(i in 1:1000){
+        counter <- counter + 1
+        if (!counter %% 5000) print(counter)
+        mtpi <- mtp[i]
+        if(length(mtpi) == 0) {
+            break
         }
-        ll[[lineage]]$seqs <- 1
-    } else {
-        for(mut in mutations) {
-            if(mut %in% names(ll[[lineage]])) {
-                old <- ll[[lineage]][[mut]]
+        mtpi <- strsplit(mtpi, "\t")[[1]]
+
+        lineage <- mtpi[20]
+        mutations <- unlist(strsplit(mtpi[46:48], ","))
+        date <- as.character(mtpi[28])
+        country <- mtpi[8]
+
+
+        if(!lineage %in% names(ll)) {
+            ll[[lineage]] <- vector(mode = "list", length = length(mutations))
+            names(ll[[lineage]]) <- mutations
+            for(mut in mutations) {
                 ll[[lineage]][[mut]] <- list(
-                    count = old$count + 1,
-                    date1 = ifelse(date < old$date1, date, old$date1),
-                    date2 = ifelse(date < old$date1, old$date1,
-                        ifelse(date < old$date2, date, old$date2)
-                    ),
-                    daten2 = ifelse(date > old$daten1, old$daten1,
-                        ifelse(date > old$daten2, date, old$daten2)
-                    ),
-                    daten1 = ifelse(date > old$daten1, date, old$daten1))
-            } else {
-            ll[[lineage]][[mut]] <- list(
-                count = 1, 
-                date1 = date, date2 = date, daten2 = date, daten1 = date)
+                    count = 1, 
+                    date1 = date, daten1 = date)
             }
+            ll[[lineage]]$seqs <- 1
+        } else {
+            for(mut in mutations) {
+                if(mut %in% names(ll[[lineage]])) {
+                    old <- ll[[lineage]][[mut]]
+                    ll[[lineage]][[mut]] <- list(
+                        count = old$count + 1,
+                        date1 = ifelse(date < old$date1, date, old$date1),
+                        daten1 = ifelse(date > old$daten1, date, old$daten1))
+                } else {
+                ll[[lineage]][[mut]] <- list(
+                    count = 1, 
+                    date1 = date, daten1 = date)
+                }
+            }
+            ll[[lineage]]$seqs <- ll[[lineage]]$seqs + 1
         }
-        ll[[lineage]]$seqs <- ll[[lineage]]$seqs + 1
     }
 }
 close(handle)
+Sys.time() - t0
 
 saveRDS(ll, here("data-raw", "ll.rds"))
 
