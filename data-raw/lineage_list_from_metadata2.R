e35feef308805de4beb78fc631033119d0e8fa4e
DBecker7 <d7becker@gmail.com>
2022-05-20 09:17:56 -0400

    Parallelize a pair of "L" eyes

--- data-raw/lineage_list_from_metadata2.R
+++ data-raw/lineage_list_from_metadata2.R
@@ -1,6 +1,10 @@
 library(lubridate)
 library(dplyr)
 library(here)
+library(parallel)
+
+ncores <- detectCores() - 4
+cl <- makeCluster(ncores)
 
 # GET the latest metadata
 # wget --show-progress -O "data-raw/metadata_$(date +"%Y-%m-%d").tsv.gz" https://data.nextstrain.org/files/ncov/open/metadata.tsv.gz
@@ -15,29 +19,30 @@ mutation_cols <- which(header %in% c("insertions", "deletions", "substitutions")
 date_submitted <- which(header == "date_submitted")
 country_col <- which(header == "country")
 sra_accession <- which(header == "sra_accession")
+clusterExport(cl, c("pango_lineage", "mutation_cols", "date_submitted"))
 
 kill_switch <- FALSE
 ll <- data.frame()
 counter <- 0
-N <- 10000
+N <- 100000
 while(!kill_switch) {
-    mtp <- strsplit(readLines(handle, n = N), split = "\t")
-    ml <- bind_rows(lapply(mtp, function(x) {
-        if(length(x) > 0) {
-            mutations <- unlist(strsplit(x[mutation_cols], ","))
-            if(length(mutations) > 0) {
-                data.frame(
-                    lineage = x[pango_lineage],
-                    mutation = mutations,
-                    date1 = as.character(x[date_submitted]),
-                    daten = as.character(x[date_submitted]),
-                    count = 1,
-                    stringsAsFactors = FALSE
-                )
+    ml <- bind_rows(parLapply(cl, 
+        strsplit(readLines(handle, n = N), split = "\t"), function(x) {
+            if(length(x) > 0) {
+                mutations <- unlist(strsplit(x[mutation_cols], ","))
+                if(length(mutations) > 0) {
+                    data.frame(
+                        lineage = x[pango_lineage],
+                        mutation = mutations,
+                        date1 = as.character(x[date_submitted]),
+                        daten = as.character(x[date_submitted]),
+                        count = 1,
+                        stringsAsFactors = FALSE
+                    )
+                }
+            } else {
+                kill_switch <<- TRUE
             }
-        } else {
-            kill_switch <<- TRUE
-        }
     }))
 
     ll <- summarise(group_by(bind_rows(ll, ml), lineage, mutation),
@@ -50,5 +55,7 @@ while(!kill_switch) {
 }
 
 mutations_by_lineage <- ll 
+
+saveRDS(ll, here("data-raw", "ll.rds"))
 
 usethis::use_data(mutations_by_lineage, overwrite = TRUE)
