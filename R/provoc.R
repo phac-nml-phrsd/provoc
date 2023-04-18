5e697f4445eaf8d4391dbb1308d9140311d0ad6b
DBecker7 <d7becker@gmail.com>
2023-04-18 16:12:13 -0400

    Allow control over verbosity of "optim" method

--- R/provoc.R
+++ R/provoc.R
@@ -7,6 +7,7 @@
 #' @param fused The fused data frame of coco and varmat. The fusion ensures that the mutations are properly joined and varmat contains only the most pertinent mutations.
 #' @param method Optimization or Bayesian?
 #' @param ncores For optim, the number of cores to be used. Requires the parallel package for ncores > 1
+#' @param update_interval Print message after \code{update_interval} samples. Set to 0 to suppress output.
 #' @param ... Arguments to be passed to \code{provoc_jags()} (ignored for method = "optim"). See \code{?provoc_jags}.
 #' 
 #' @return Results of the estimation. Regardless of the technique used, the results are as follows.
@@ -27,7 +28,7 @@
 #' fused <- fuse(coco, varmat)
 #' res <- provoc(fused)
 #' res$point_est
-provoc <- function (coco, varmat, fused = NULL, method = c("optim", "runjags"), ncores = 1, bootstrap_samples = 0, ...) {
+provoc <- function (coco, varmat, fused = NULL, method = c("optim", "runjags"), ncores = 1, bootstrap_samples = 0, update_interval = 20, ...) {
     if(is.null(fused)) fused <- fuse(coco, varmat)
     if("sample" %in% colnames(fused)) {
         samples <- unique(fused$sample)
@@ -56,12 +57,17 @@ provoc <- function (coco, varmat, fused = NULL, method = c("optim", "runjags"), 
     convergence <- logical(length(samples))
     t0s <- double(length(samples))
     names(res_list) <- samples
-    for(i in seq_along(res_list)) { # TODO: Parallelize (for optim)
+    if (update_interval) message(paste0("Fitting ", length(samples), " samples."))
+    for (i in seq_along(res_list)) { # TODO: Parallelize (for optim)
         t0 <- Sys.time()
-        cat("\n")
-        message(paste0("Fitting sample ", samples[i], ", ", 
-            which(samples == samples[i]), " of ", length(samples)))
-        
+        if (update_interval){
+            if (!i %% update_interval) {
+                cat("\n")
+                message(paste0("Fitting sample ", samples[i], ", ",
+                    which(samples == samples[i]), " of ",
+                    length(samples)))
+            }
+        }
         fissed <- fission(fused, sample = samples[i])
         coco <- fissed$coco
         varmat <- fissed$varmat
@@ -106,7 +112,6 @@ provoc <- function (coco, varmat, fused = NULL, method = c("optim", "runjags"), 
         # TODO: Replace processing in provoc() with separate processing functions for optim and jags; user can choose how to process.
         # TODO: Include median read depth, quality measures
 
-        message("Done.")
         t0s[i] <- difftime(Sys.time(), t0, units = "mins")
     }
 
