edc4ee6dc25edac55c9ef7206636ff4a11737a26
Jeremy Faria <fari9860@mylaurier.ca>
2024-01-08 16:10:15 -0500

    removed method = "jags" option in provoc function and edited/removed documentation relevant to that removal

--- R/provoc.R
+++ R/provoc.R
@@ -5,19 +5,17 @@
 #' @param coco The counts and coverage data frame. Ignored if \code{fused} is supplied.
 #' @param varmat The matrix of variant mutations. Ignored if \code{fused} is supplied.
 #' @param fused The fused data frame of coco and varmat. The fusion ensures that the mutations are properly joined and varmat contains only the most pertinent mutations.
-#' @param method Optimization or Bayesian?
 #' @param ncores For optim, the number of cores to be used. Requires the parallel package for ncores > 1
 #' @param update_interval Print message after \code{update_interval} samples. Set to 0 to suppress output.
-#' @param ... Arguments to be passed to \code{provoc_jags()} (ignored for method = "optim"). See \code{?provoc_jags}.
 #' 
 #' @return Results of the estimation. Regardless of the technique used, the results are as follows.
 #' 
 #' \describe{
-#'      \item{point_est}{A data frame with columns labelled rho, ci_low, ci_high, and the name of the variant. For \code{optim}, these are the estimates (bootstrap CI not yet implemented). For \code{runjags}, these are the medians and quantiles.}
+#'      \item{point_est}{A data frame with columns labelled rho, ci_low, ci_high, and the name of the variant. These are the estimates (bootstrap CI not yet implemented).}
 #'      \item{convergence}{True if the algorithm converged.}
-#'      \item{convergence_notes}{For \code{optim}, the message returned from \code{optim} as well as the initialization method for rho. For \code{runjags}, the gelman-rubin statistics for each parameter.}
+#'      \item{convergence_notes}{the message returned from \code{optim} as well as the initialization method for rho.}
 #'      \item{note}{A brief note about the method.}
-#'      \item{loglik}{The log likelihood value. For \code{runjags}, this is calculated for the medians of all parameters. This may be changed in future updates.}
+#'      \item{loglik}{The log likelihood value. This may be changed in future updates.}
 #'      \item{sample_info}{The function checks for any columns that have exactly one unique value within a given sample. The unique value of each is returned in a data frame. Very useful if samples have columns such as "date" or "location".}
 #' }
 #' @export
@@ -28,7 +26,7 @@
 #' fused <- fuse(coco, varmat)
 #' res <- provoc(fused)
 #' res$point_est
-provoc <- function (coco, varmat, fused = NULL, method = c("optim", "runjags"), ncores = 1, bootstrap_samples = 0, update_interval = 20, verbose = TRUE, ...) {
+provoc <- function (coco, varmat, fused = NULL, ncores = 1, bootstrap_samples = 0, update_interval = 20, verbose = TRUE) {
     if(is.null(fused)) fused <- fuse(coco, varmat)
     if("sample" %in% colnames(fused)) {
         samples <- unique(fused$sample)
@@ -75,36 +73,15 @@ provoc <- function (coco, varmat, fused = NULL, method = c("optim", "runjags"), 
         # Guaranteed to include a column called "sample"
         sample_info <- apply(coco, 2, function(x) length(unique(x)) == 1)
         sample_info <- coco[1, sample_info, drop = FALSE]
-
-
-        if(method[1] == "optim") {
-            res_temp <- provoc_optim(coco, varmat, bootstrap_samples = bootstrap_samples, verbose = verbose)
-            res_df <- res_temp$res_df
-            for (ii in seq_len(ncol(sample_info))) {
-                res_df[, names(sample_info)[ii]] <- sample_info[1, ii]
-            }
-            convergence[i] <- res_temp$convergence
-            convergence_note[i] <- res_temp$convergence_note
-            res_list[[i]] <- res_df
-        } else {
-            if(requireNamespace("runjags", quietly = TRUE)) {
-                if(!runjags::testjags()$JAGS.found) {
-                    stop("runjags was unable to find an installation of JAGS. If you believe JAGS is installed, try running `library(rjags)`.")
-                }
-                res_temp <- provoc_jags(coco, varmat, ...)
-                res_df <- res_temp$res_df
-                for (ii in seq_len(ncol(sample_info))) {
-                    res_df[, names(sample_info)[ii]] <- sample_info[1, ii]
-                }
-                convergence[i] <- res_temp$convergence
-                convergence_note[i] <- res_temp$convergence_note
-                res_list[[i]] <- res_df
-                    
-            } else {
-                res_df <- "JAGS and runjags are required to use this method."
-                res_list[[i]] <- data.frame(note = res_df)
-            }
+        
+        res_temp <- provoc_optim(coco, varmat, bootstrap_samples = bootstrap_samples, verbose = verbose)
+        res_df <- res_temp$res_df
+        for (ii in seq_len(ncol(sample_info))) {
+            res_df[, names(sample_info)[ii]] <- sample_info[1, ii]
         }
+        convergence[i] <- res_temp$convergence
+        convergence_note[i] <- res_temp$convergence_note
+        res_list[[i]] <- res_df
         # TODO: Add methods (for both single results and lists): summary, plot
             # Only print top variants; include columns that are constant within samples; convergence status; log-Likelihood
             # Autoplot (gg) for res and res_list objects?
