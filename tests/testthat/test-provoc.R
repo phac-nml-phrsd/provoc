a1cba7843e92b309a6ffe68b90f423b632cd6ebf
Jeremy Faria <fari9860@mylaurier.ca>
2024-01-21 10:39:59 -0500

    Bootstrap CI unit test for Provoc completed

---
+++ tests/testthat/test-provoc.R
@@ -0,0 +1,21 @@
+test_that("Ensure estimate is within the bootstrap confidence interval.", {
+  expect_results <- function(converg_info){
+    i = 1
+    while(i <= nrow(converg_info)){ #iterates through each row of res_df
+      expect_lt(converg_info$rho[i], converg_info$ci_high[i]) #checks to see if rho is less than the upper bound of the CI
+      expect_gt(converg_info$rho[i], converg_info$ci_low[i]) #checks to see if rho is less than the lower bound of the CI
+      i = i + 1
+    }
+  }
+  test_bootstrap <- function(rel_counts){
+    varmat <- simulate_varmat() #simulates a new variant matrix
+    coco <- simulate_coco(varmat, rel_counts)
+    converg_info <- provoc(coco, varmat, NULL, 1, 1000, 20, TRUE) #estimates the proportions of VOCs with 1000 bootstrap samples
+    expect_results(converg_info)
+  }
+  j = 0
+  while(j < 10){ #simulates data 10 different times with a relative counts of each VOC (60 unit tests total)
+    test_bootstrap(sample.int(1000,3,replace=TRUE)) #takes a random array of integers up to 1000 of length 3
+    j = j + 1
+  }
+})
