4d2c679e0bb93e21f3e50ce83d9c6f4be5eec5c9
Jeremy Faria <fari9860@mylaurier.ca>
2024-01-24 11:19:27 -0500

    added comments to unit test

--- tests/testthat/test-provoc.R
+++ tests/testthat/test-provoc.R
@@ -21,18 +21,18 @@ test_that("Ensure estimate is within the bootstrap confidence interval.", {
 })
 
 test_that("rho total equals 1", {
+  epsilon = 0.005 #initializes epsilon, put outside of rho_test function since it doesn't change
   rho_test <- function(converg_info){
-    epsilon = 0.005
     i = 1
     rho_total = 0
     while(i <= nrow(converg_info)){
-      expect_lt(converg_info$rho[i], 1)
-      expect_gt(converg_info$rho[i], 0)
+      expect_lt(converg_info$rho[i], 1) #checks if each rho is less than 1
+      expect_gt(converg_info$rho[i], 0) #checks if each rho is greater than 1
       rho_total = rho_total + converg_info$rho[i]
       i = i + 1
     }
-    expect_lt(rho_total, 1+epsilon)
-    expect_gt(rho_total, 1-epsilon)
+    expect_lt(rho_total, 1+epsilon) #checks to see if rho total is between 1-epsilon and 1+epsilon, 
+    expect_gt(rho_total, 1-epsilon) #since it should equal 1 but addition with significant digits can cause issues on preciseness.
   }
   create_provoc_table <- function(rel_counts){
     varmat <- simulate_varmat() #simulates a new variant matrix
