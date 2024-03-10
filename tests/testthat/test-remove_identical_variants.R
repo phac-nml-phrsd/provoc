73fd90c09e1162dc062bc21e9dfdb8dff87de11d
Jeremy Faria <fari9860@mylaurier.ca>
2024-03-10 10:52:49 -0400

    updated remove identical variant test file

--- tests/testthat/test-remove_identical_variants.R
+++ tests/testthat/test-remove_identical_variants.R
@@ -11,3 +11,17 @@ test_that("ensuring only unique variants are kept", {
   updated_fused_df <- provoc:::remove_identical_variants(fused_df, annihilate = TRUE)
   expect_equal(ncol(updated_fused_df), 6)
 })
+
+test_that("ensuring when annilihate is false, no columns are removed", {
+  varmat <- simulate_varmat()
+  coco <- simulate_coco(varmat)
+  vardf <- as.data.frame(t(varmat))
+  names(vardf) <- paste0("var_", names(vardf))
+  vardf$mutation <- rownames(vardf)
+  fused_df <- dplyr::left_join(coco, vardf, by = "mutation")
+  fused_df$var_BA.3 <- as.vector(fused_df$var_BA.1)
+  fused_df$var_BA.4 <- as.vector(fused_df$var_BA.2)
+  fused_df$var_BA.5 <- as.vector(fused_df$var_BA.2)
+  updated_fused_df <- provoc:::remove_identical_variants(fused_df, annihilate = FALSE)
+  expect_equal(ncol(updated_fused_df), 9)
+})
