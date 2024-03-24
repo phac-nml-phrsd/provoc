4afdbfa04fdb04d7e54a50b848b75689c51996d3
Jeremy Faria <fari9860@mylaurier.ca>
2024-03-24 10:43:35 -0400

    updated remove identical variants to account for no duplicate variants in the dataframe

--- R/provoc.R
+++ R/provoc.R
@@ -134,8 +134,10 @@ remove_identical_variants <- function(fused_df, annihilate){
     subset_of_variants <- dplyr::select(fused_df,contains("var_"))
     if (annihilate) {
         unique_subset_of_variants <- t(unique(t(subset_of_variants)))
-        print(paste0("Variant ", colnames(subset_of_variants)[!(colnames(subset_of_variants) %in% colnames(unique_subset_of_variants))],
+        if (ncol(subset_of_variants) != ncol(unique_subset_of_variants)) {
+          print(paste0("Variant ", colnames(subset_of_variants)[!(colnames(subset_of_variants) %in% colnames(unique_subset_of_variants))],
                        " is a duplicate variant and has been removed from the dateframe"))
+        }
         return(cbind(dplyr::select(fused_df, !contains("var_")), unique_subset_of_variants))
     }
     else {
